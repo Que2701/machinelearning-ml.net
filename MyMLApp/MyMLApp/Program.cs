4eabd66326a17670f7412fed619b015166c28b04
tumelo <tumelom@evolvemed.co.za>
2020-08-08 23:56:16 +0200

    intro into ml.net

---
+++ MyMLApp/MyMLApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MyMLAppML.Model;
+
+namespace MyMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "this is rude";
+            // Load model and predict output of sample data
+            ModelOutput modelOutput = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {modelOutput.Prediction}");
+
+            Console.WriteLine("ML.Net");
+        }
+    }
+}
