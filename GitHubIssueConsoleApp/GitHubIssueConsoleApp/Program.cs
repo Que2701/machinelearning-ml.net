b1bc7a05ecee10c3f5fa09d3d0775f6eda444cf4
tumelo <tumelom@evolvemed.co.za>
2020-08-09 09:07:38 +0200

    added a gitignore and attributes file

---
+++ GitHubIssueConsoleApp/GitHubIssueConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubIssueConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
